eee8341c568a7496e6ae0474eafb2847d84b3642
Madaeli <myvaneli@gmail.com>
2023-06-06 10:58:45 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,64 @@
+import { Text,ScrollView, View} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from  "react-native-safe-area-context";
+import { s } from "./app.style";
+import { Header } from "./components/Header/Header.jsx";
+import { CardTodo } from "./components/CardTodo/CardTodo.jsx"
+import {useState} from "react";
+import { TapBottomMenu } from "./components/TapeBottomMenu/TapBottomMenu.jsx"
+export default function App() { 
+    const [ selectedTabName, setSelectedTabName ] = useState("all");
+
+    const [todoList, setTodoList]= useState([
+      {id: 1, title : "Sortir le chien", isCompleted: true },
+      {id: 2, title : "Aller chez le garagiste", isCompleted: false },
+      {id: 3, title : "Faire les courses", isCompleted: true },
+      {id: 4, title : "Appeler le vétérinaire", isCompleted: true },
+      {id: 5, title : "Sortir le chien", isCompleted: true },
+      {id: 6, title : "Aller chez le garagiste", isCompleted: false },
+      {id: 7, title : "Faire les courses", isCompleted: true },
+      {id: 8, title : "Appeler le vétérinaire", isCompleted: true },
+    ]);
+    
+  function updateTodo(todo){
+    const updateTodo = {
+      ...todo,
+      isCompleted: !todo.isCompleted
+    };
+
+    const indexToUpdate =todoList.findIndex(
+        (todo)=> todo.id=== updateTodo.id 
+    );
+
+    const updateTodoList = [...todoList]
+    updateTodoList[indexToUpdate]= updateTodo;
+    setTodoList(updateTodoList);
+  }
+
+  function renderTodoList(){
+      return todoList.map((todo)=>( 
+            <View style={s.cardItem} key={todo.id}> 
+                  <CardTodo onPress ={updateTodo} todo={todo} />
+            </View>));
+    }
+
+    return(
+          <>
+            <SafeAreaProvider>
+                  <SafeAreaView style = { s.app }>
+                        <View style={s.header}>
+                          <Header/> 
+                        </View>
+                        <View style={s.body}>
+                          <ScrollView> 
+                            {renderTodoList()} 
+                          </ScrollView>
+                         
+                        </View>                 
+                  </SafeAreaView>  
+            </SafeAreaProvider>
+            <View style={s.footer}>
+                  <TapBottomMenu selectedTabName = { selectedTabName } />
+          </View>
+          </>       
+      );
+}
